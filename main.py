ad2bfd6bdbe090bf3e6071fc281d97c063b1be5d
damczi17 <72543223+damczi17@users.noreply.github.com>
2022-05-09 18:58:16 +0200

    Unbalanced issue updated

--- main.py
+++ main.py
@@ -13,20 +13,20 @@ def transport_cost(koszty_transportu, plan_transportu):
 
 def equation_solve(alfa, i, beta, j, koszty_transportu):
     if(alfa[i] == 0):
-        alfa[i] = -1*(beta[j]+koszty_transportu)
+        alfa[i] = -1*(beta[j] + koszty_transportu)
     if(beta[j] == 0):
-        beta[j] = -1*(alfa[i]+koszty_transportu)
+        beta[j] = -1 * (alfa[i] + koszty_transportu)
     
 def optimal_solution(koszty_transportu, plan_transportu):
-    alfa = np.zeros(len(koszty_transportu))
-    beta = np.zeros(len(koszty_transportu[0]))
+    alfa = np.linspace(1, 1, num = len(koszty_transportu))
+    beta = np.linspace(1, 1, num=len(koszty_transportu[0]))
 
     for i in reversed(range(len(koszty_transportu))):
         for j in reversed(range(len(koszty_transportu[i]))):
             if(plan_transportu[i][j] != 0):
-                if(i == len(koszty_transportu)):
+                if(i == len(koszty_transportu)-1):
                     beta[j] = -1 *  koszty_transportu[i][j]
-
+                    alfa[i] = 0.
                 else:
                     equation_solve(alfa, i, beta, j, koszty_transportu[i][j])
 
